1a6f0c3cc3bc17b68b269223f9a307f901d63806
MrChuse <43815531+MrChuse@users.noreply.github.com>
2021-08-05 16:38:19 +0300

    Added Add methods

--- Assets/Client.cs
+++ Assets/Client.cs
@@ -1,17 +1,31 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.IO;
 using UnityEngine;
+using System.Text;
 
 public class Client : MonoBehaviour
 {
-    public const int port = 9002;
+    public const int port = 9003;
     public const string server = "127.0.0.1";
     TcpClient client;
     NetworkStream stream;
-    
+
+    List<int> types;
+    List<int> lengths;
+    MemoryStream data;
+    int totalLength;
+    enum Types
+    {
+        NOTSUPPORTED = 0,
+        INT = 1,
+        FLOAT = 2,
+        BOOL = 3,
+        STRING = 4
+    }
+
     void ConnectClient()
     {
         client = new TcpClient();
@@ -27,8 +41,11 @@ public class Client : MonoBehaviour
     
     void Start()
     {
+        totalLength = 0;
+        types = new List<int>();
+        lengths = new List<int>();
+        data = new MemoryStream();
         ConnectClient();
-        
     }
 
     void Update()
@@ -37,8 +54,12 @@ public class Client : MonoBehaviour
         stream.Write(want_to_send, 0, want_to_send.Length);
         // send true to indicate that you want to send data
         // and server won't close the connection
-        SendList(new int[]{Time.frameCount});
-        
+        Add(Time.frameCount);
+        Add(1.0f * Time.frameCount / 10);
+        Add(Time.frameCount % 2 == 0);
+        Add(Time.frameCount + 2);
+        Add(Time.frameCount.ToString());
+        SendFrameData();
     }
     
     void OnApplicationQuit()
@@ -49,11 +70,63 @@ public class Client : MonoBehaviour
         CloseClient();
     }
     
-    void SendList(int[] list){
+    void SendArray(int[] list){
         byte[] list_bytes = new byte[list.Length * sizeof(int)];
         byte[] list_bytes_length = BitConverter.GetBytes(list_bytes.Length);
         Buffer.BlockCopy(list, 0, list_bytes, 0, list_bytes.Length);
         stream.Write(list_bytes_length, 0, list_bytes_length.Length);
         stream.Write(list_bytes, 0, list_bytes.Length);
+    }
+
+
+    void Add(string n){
+        byte[] nBytes = Encoding.ASCII.GetBytes(n);
+        data.Write(nBytes, 0, nBytes.Length);
+
+        totalLength += nBytes.Length;
+        types.Add((int)Types.STRING);
+        lengths.Add(nBytes.Length);
+    }
+    void Add(bool n)
+    {
+        byte[] nBytes = BitConverter.GetBytes(n);
+        data.Write(nBytes, 0, nBytes.Length);
+
+        totalLength += nBytes.Length;
+        types.Add((int)Types.BOOL);
+        lengths.Add(nBytes.Length);
+    }
+    void Add(int n) {
+        byte[] nBytes = BitConverter.GetBytes(n);
+        data.Write(nBytes, 0, nBytes.Length);
+
+        totalLength += nBytes.Length;
+        types.Add((int)Types.INT);
+        lengths.Add(nBytes.Length);
+    }
+    void Add(float n)
+    {
+        byte[] nBytes = BitConverter.GetBytes(n);
+        data.Write(nBytes, 0, nBytes.Length);
+
+        totalLength += nBytes.Length;
+        types.Add((int)Types.FLOAT);
+        lengths.Add(nBytes.Length);
+    }
+
+    void SendFrameData()
+    {
+        byte[] a = data.ToArray();
+        for(int i = 0; i < a.Length; i++)
+        {
+            Debug.Log(Time.frameCount.ToString() + "   " + i.ToString() + "th byte " + a[i]);
+        }
+        SendArray(types.ToArray()); // send all the types
+        SendArray(lengths.ToArray()); // send corrensponding lengths;
+        byte[] data_bytes = data.ToArray();
+        stream.Write(data_bytes, 0, data_bytes.Length); // send the data itself
+        types.Clear();
+        lengths.Clear();
+        data.Seek(0, SeekOrigin.Begin);
     }
 }
